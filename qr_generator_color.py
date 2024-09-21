61e65b93d4737784f25511294c5a4a10a5cf551c
Aayush Raj Karan <111561581+1-ayu@users.noreply.github.com>
2024-09-22 00:19:24 +0530

    Add files via upload

---
+++ qr_generator_color.py
@@ -0,0 +1,10 @@
+import qrcode
+from PIL import Image
+import qrcode.constants
+qr=qrcode.QRCode(version=3,
+                 error_correction=qrcode.constants.ERROR_CORRECT_H,
+                 box_size=10,border=5)
+qr.add_data("https://www.youtube.com/watch?v=ekwPDGyd79s")
+qr.make(fit=True)
+img=qr.make_image(fill_color="white",back_color="black")
+img.save("Ayush1.png")
